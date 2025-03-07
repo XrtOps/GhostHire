fa6506967e0ffb5607232b96bde71460dd813935
Seongwon Cho <scho5929@conestogac.on.ca>
2025-03-07 18:29:05 -0500

    Design Login Page(Signup page will be work in progress later on)

---
+++ GhostHire/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GhostHire.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUpPage()
+        {
+            return View();
+        }
+
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+    }
+}
--- GhostHire/obj/Debug/net9.0/GhostHire.AssemblyInfo.cs
+++ GhostHire/obj/Debug/net9.0/GhostHire.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GhostHire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4751583029d509c0a22b36ae706155cade5433")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa0a42c92a40090df7cbe64fd3c196299bd2d3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GhostHire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GhostHire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
